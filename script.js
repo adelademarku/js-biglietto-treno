766223d0e2ec078d1a55a31b37a7a081818881cb
adelademarku <adelademarku@email.com>
2022-11-10 15:03:22 +0100

    prompt age e km

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let kilometres = prompt("Insert kilometres");
+let age =prompt("Insert age");
+
+
+
+console.log("kilometres:  " + kilometres);
+console.log("age:  " + age);
+
+kilometres = parseInt(kilometres);
+console.log("Quanto vale numero1: " + kilometres);
+
+age = parseInt(age);
+console.log("Quanto vale numero2: " + age);
+
+if ((age <= 0) || (age > 200)) {
+    document.write("Errore Age");
+    }
+
+let kilometresPrice = kilometres * 0.21
+
+  console.log("price kilometres: " + kilometresPrice);
+
+
+
+
+
+
+
